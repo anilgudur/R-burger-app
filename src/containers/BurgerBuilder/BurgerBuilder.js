d78e69b637ad6b643530e914e5e1728056833cff
anilg-clarion <anil.gudur@clariontechnologies.co.in>
2019-01-11 18:20:18 +0530

    8.119 - Burger component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
+import Burger from "../../components/Burger/Burger";
 
 class BurgerBuilder extends Component {
   render() {
     return (
       <>
-        <div>Burger image</div>
+        <Burger />
         <div>Burger controls</div>
       </>
     );
