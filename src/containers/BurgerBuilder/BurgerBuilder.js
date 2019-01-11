168bd428568bcca403fad62c63093e67f214d063
anilg-clarion <anil.gudur@clariontechnologies.co.in>
2019-01-11 16:20:11 +0530

    8.116 - Burger Builder implementation

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class BurgerBuilder extends Component {
+  render() {
+    return (
+      <>
+        <div>Burger image</div>
+        <div>Burger controls</div>
+      </>
+    );
+  }
+}
+
+export default BurgerBuilder;
