387eaa1c090aaeed7ec8a0a8ee5e6160c4288734
anilgudur <anilg.cse@gmail.com>
2019-02-02 22:25:46 +0530

    12.216 - + converted to number

--- src/containers/Checkout/CheckoutContainer.js
+++ src/containers/Checkout/CheckoutContainer.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import CheckoutSummary from "../../components/Order/CheckoutSummary/CheckoutSummary";
 
-class Checkout extends Component {
+class CheckoutContainer extends Component {
   state = {
     ingredients: {
       salad: 1,
@@ -15,9 +15,10 @@ class Checkout extends Component {
     const query = new URLSearchParams(this.props.location.search);
     const ingredients = {};
     for (let param of query.entries()) {
-      ingredients[param[0]] = param[1];
+      ingredients[param[0]] = +param[1];
     }
-    this.setState({ ingredients });
+    console.log("ingredients: ", ingredients);
+    this.setState({ ingredients: ingredients });
   }
 
   onCheckoutCancel() {
@@ -41,4 +42,4 @@ class Checkout extends Component {
   }
 }
 
-export default Checkout;
+export default CheckoutContainer;
